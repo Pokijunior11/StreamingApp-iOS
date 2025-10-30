2a213ae74a7543cf4e9c5ef66fed99e9c6d00670
Lovro <lovro@binaria.io>
2025-10-30 14:12:45 +0100

    Initial Commit

---
+++ agora/agoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  agoraApp.swift
+//  agora
+//
+//  Created by Binaria on 30.10.2025..
+//
+
+import SwiftUI
+
+@main
+struct agoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
